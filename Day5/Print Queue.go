658f4fa0155a2653463eaa38c6881ef383573fc0
chongms <chongms@sg.ibm.com>
2024-12-09 20:30:42 +0800

    completed day 5

--- Day5/Print Queue.go
+++ Day5/Print Queue.go
@@ -6,10 +6,17 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-type Pair struct {
-	X, Y int
+// type PageRule struct {
+// 	before int
+// 	after  int
+// }
+
+type PageOrder struct {
+	lefts  []int
+	rights []int
 }
 
 func main() {
@@ -23,8 +30,7 @@ func main() {
 	}
 	scanner := bufio.NewScanner(file)
 	var update []string
-	var pageList []Pair
-	intSet := make(map[int]struct{})
+	pageMap := make(map[int]PageOrder)
 	for scanner.Scan() {
 		line := scanner.Text()
 		re := regexp.MustCompile(`(\d+)\|(\d+)`)
@@ -34,68 +40,95 @@ func main() {
 		if matches != nil {
 			x, _ := strconv.Atoi(matches[1])
 			y, _ := strconv.Atoi(matches[2])
-			c := Pair{X: x, Y: y}
+			// p := PageRule{before: x, after: y}
+			// pageRules = append(pageRules, p)
 
-			if _, exists := intSet[x]; !exists {
-				intSet[x] = struct{}{}
-			}
-			if _, exists := intSet[y]; !exists {
-				intSet[y] = struct{}{}
-			}
+			pageRulesX := pageMap[x]
+			rightRules := AddRule(pageRulesX.rights, y)
+			newXRules := PageOrder{lefts: pageRulesX.lefts, rights: rightRules}
+			pageMap[x] = newXRules
 
-			pageList = append(pageList, c)
+			pageRulesY := pageMap[y]
+			leftRules := AddRule(pageRulesY.lefts, x)
+			newYRules := PageOrder{lefts: leftRules, rights: pageRulesY.rights}
+			pageMap[y] = newYRules
+
 		}
 
 		update = append(update, reComplete.FindAllString(line, -1)...)
 
 	}
 
-	sortedList := SortPage(intSet, pageList)
+	//fmt.Printf("Answer1: %v\n", pageRules)
+	//fmt.Printf("Answer2: %v\n", update)
+	//fmt.Printf("Answer3: %v\n,", pageMap)
 
-	fmt.Printf("Answer1: %v\n", pageList)
-	fmt.Printf("Answer2: %v\n", update)
-	fmt.Printf("Answer1: %v\n", intSet)
-}
+	//list := SortPages(pageRules)
+	//list.Print()
 
-func SortPage(intMap map[int]struct{}, pagePair []int) []int {
-	var sortedInt []int
-	for key := range intMap {
-		if len(sortedInt) < 1 {
-			sortedInt = append(sortedInt, key)
+	var sum, fixedSum int
+	newList := make([][]string, 0)
+	for _, value := range update {
+		pages := strings.Split(value, ",")
+		midpoint, wrongList := CheckPageRule(pageMap, pages)
+		if midpoint < 0 {
+			newList = append(newList, wrongList)
 		} else {
-			for 
+			sum = sum + midpoint
 		}
 	}
+
+	for i := 0; i < len(newList); i++ {
+		midpoint, wrongList := CheckPageRule(pageMap, newList[i])
+		if midpoint < 0 {
+			newList = append(newList, wrongList)
+		} else {
+			fixedSum = fixedSum + midpoint
+		}
+	}
+
+	fmt.Printf("Answer1: %d\n", sum)
+	fmt.Printf("Answer2: %d\n", fixedSum)
 }
 
-// func SortPage(list []string) []int {
-// 	var sortedInt []int
-// 	for i := range list {
-// 		reInt := regexp.MustCompile(`\d+`)
-// 		matches := reInt.FindAllString(i, -1)
-// 		x, _ := strconv.Atoi(matches[0])
-// 		y, _ := strconv.Atoi(matches[1])
+func AddRule(slice []int, value int) []int {
+	for _, v := range slice {
+		if v == value {
+			return slice
+		}
+	}
+	return append(slice, value)
+}
 
-// 		if len(sortedInt) < 1 {
-// 			sortedInt = append(sortedInt, x, y)
-// 		} else {
-// 			for j := range sortedInt {
-// 				if Compare(x, j) {
-// 					sortedInt = append(sortedInt, x)
-// 				}
-// 			}
-// 		}
+func Exist(slice []int, value int) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
 
-// 	}
+func CheckPageRule(rules map[int]PageOrder, pages []string) (int, []string) {
 
-// }
+	var found bool
+	for i := 0; i < len(pages)-1; i++ {
+		current, _ := strconv.Atoi(pages[i])
+		next, _ := strconv.Atoi(pages[i+1])
 
-// func Compare(x, y int) bool{
+		rights := rules[current].rights
+		if !Exist(rights, next) {
+			found = true
+			//SwapThem
+			pages[i] = strconv.Itoa(next)
+			pages[i+1] = strconv.Itoa(current)
+		}
+	}
 
-// 	if ()
+	if found {
+		return -1, pages
+	}
 
-// }
-
-// func CheckUpdate(pair Pair, input []string) []int {
-
-// }
+	mid, _ := strconv.Atoi(pages[len(pages)/2])
+	return mid, pages
+}
